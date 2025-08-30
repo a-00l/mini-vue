6487538cbd8e2ed95447ff53c9f3fd733e9942c3
雪海-花 <2010433856@qq.com>
2025-08-30 18:19:26 +0800

    build：引入vite、vitest依赖

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    // 测试相关配置
+    globals: true, // 允许使用全局测试函数（如 test, expect）
+    environment: 'jsdom', // 模拟浏览器环境，适合测试 DOM 相关代码
+  },
+});
